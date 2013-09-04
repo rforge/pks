e7df9c6d3f93270434293dd015d25a3a4a52f0b5
fwickel <fwickel@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-09-04 11:01:24 +0000

    add as.set argument to as.pattern()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pks@135 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/blim.R
+++ pkg/R/blim.R
@@ -261,12 +261,12 @@ simulate.blim <- function(object, nsim = 1, seed = NULL, ...){
 
 
 ## Convert binary matrix to vector of response patterns
-as.pattern <- function(R, freq = FALSE, as.letters = FALSE){
+as.pattern <- function(R, freq = FALSE, as.letters = FALSE, as.set = FALSE){
   if(freq){
     N.R <- table(apply(R, 1, paste, collapse=""))
     setNames(as.integer(N.R), names(N.R))          # convert to named int
   }else
-    if(as.letters){
+    if(as.letters | as.set){
       nitems <- ncol(R)
       item.names <- 
        make.unique(c("a", letters[(seq_len(nitems) %% 26) + 1])[-(nitems + 1)],
@@ -274,7 +274,17 @@ as.pattern <- function(R, freq = FALSE, as.letters = FALSE){
       lett <- apply(R, 1, function(r) paste(item.names[which(r == 1)],
                     collapse=""))
       lett[lett == ""] <- "0"
-      lett
+
+      if(as.set){
+        # Separate elements in lett by "_", remove leading/trailing "_",
+        # then strsplit along "_"
+        setfam <- as.set(sapply(strsplit(
+          gsub("^_(.+)_$", "\\1", gsub("([0-9]*)", "\\1_", unname(lett))),
+          "_"), as.set))
+        setfam[[set("0")]] <- set()  # proper empty set
+        setfam  # return family of sets, class set
+      }else
+        lett    # return letters, class character
     }else
       unname(apply(R, 1, paste, collapse=""))
 }
