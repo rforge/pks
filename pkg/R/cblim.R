fca3c2f4ddd851387e270946737b33d76613fcea
fwickel <fwickel@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-04-17 13:52:20 +0000

    delineate improvements
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pks@151 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/cblim.R
+++ pkg/R/cblim.R
@@ -1,9 +1,9 @@
 ## Delineate knowledge structure by skill function
-delineate <- function(skillfun, itemID = "item") {
-  stopifnot(itemID %in% colnames(skillfun))
+delineate <- function(skillfun, itemID = 1) {
 
   item.names <- as.character(unique(skillfun[, itemID]))
-  mu <- t(as.matrix(skillfun[, colnames(skillfun) != itemID])) # numeric part
+# mu <- t(as.matrix(skillfun[, colnames(skillfun) != itemID]))
+  mu <- t(as.matrix(skillfun[, -itemID]))  # numeric part
   nskills <- nrow(mu)
   T <- as.matrix(expand.grid(rep(list(0:1), nskills)))
   colnames(T) <- rownames(mu)
@@ -18,7 +18,7 @@ delineate <- function(skillfun, itemID = "item") {
   rownames(K) <- as.pattern(K)
   colnames(K) <- item.names
   classes <- lapply(seq_len(nrow(K)),
-                    function(i) T[grep(rownames(K)[i], pat.del.states), ])
+                 function(i) rbind(T[grep(rownames(K)[i], pat.del.states), ]))
   names(classes) <- rownames(K)
   list(K = K, classes = classes)
 }
